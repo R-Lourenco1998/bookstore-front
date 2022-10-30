7c47fe9645b1142f6e2b5fe752e44676deea36cd
Luiz Ricardo Lourenço da Silva <46008216+R-Lourenco1998@users.noreply.github.com>
2022-10-30 14:23:27 -0300

    update de livro

--- src/app/components/views/livro/livro-update/livro-update.component.ts
+++ src/app/components/views/livro/livro-update/livro-update.component.ts
@@ -5,11 +5,11 @@ import { Livro } from "../livro.model";
 import { LivroService } from "../livro.service";
 
 @Component({
-  selector: "app-livro-create",
-  templateUrl: "./livro-create.component.html",
-  styleUrls: ["./livro-create.component.css"],
+  selector: "app-livro-update",
+  templateUrl: "./livro-update.component.html",
+  styleUrls: ["./livro-update.component.css"],
 })
-export class LivroCreateComponent implements OnInit {
+export class LivroUpdateComponent implements OnInit {
   titulo = new FormControl("", [Validators.minLength(3)]);
   nome_autor = new FormControl("", [Validators.minLength(3)]);
   texto = new FormControl("", [Validators.minLength(10)]);
@@ -29,20 +29,44 @@ export class LivroCreateComponent implements OnInit {
 
   ngOnInit(): void {
     this.id_cat = this.route.snapshot.paramMap.get("id_cat")!;
+    this.livro.id = this.route.snapshot.paramMap.get("id")!;
+    this.findById();
   }
 
   create(): void {
-    this.livroService.create(this.livro, this.id_cat).subscribe((resposta) => {
-      this.router.navigate([`categorias/${this.id_cat}/livros`]);
-      this.livroService.mensagem("Livro criado com sucesso!");
-    }, err => {
-      this.router.navigate([`categorias/${this.id_cat}/livros`]);
-      this.livroService.mensagem("Erro ao criar novo Livro!");
-    });
+    this.livroService.create(this.livro, this.id_cat).subscribe(
+      (resposta) => {
+        this.router.navigate([`categorias/${this.id_cat}/livros`]);
+        this.livroService.mensagem("Livro criado com sucesso!");
+      },
+      (err) => {
+        this.router.navigate([`categorias/${this.id_cat}/livros`]);
+        this.livroService.mensagem("Erro ao criar novo Livro!");
+      }
+    );
   }
 
   cancel(): void {
     this.router.navigate([`categorias/${this.id_cat}/livros`]);
+  }
+
+  findById(): void {
+    this.livroService.findById(this.livro.id!).subscribe((resposta) => {
+      this.livro = resposta;
+    });
+  }
+
+  update(): void {
+    this.livroService.update(this.livro).subscribe(
+      (resposta) => {
+        this.router.navigate([`categorias/${this.id_cat}/livros`]);
+        this.livroService.mensagem("Livro atualizado com sucesso!");
+      },
+      (err) => {
+        this.router.navigate([`categorias/${this.id_cat}/livros`]);
+        this.livroService.mensagem("Erro ao atualizar Livro!");
+      }
+    );
   }
 
   getMessage() {
